dead20235a1963ac79c8dc67da5f5b5720826c8a
anuradhabamane <150005209+anuradhabamane@users.noreply.github.com>
2023-11-14 14:36:48 +0530

    Add files via upload

---
+++ routes/ToDoRoute.js
@@ -0,0 +1,12 @@
+// const {Router} = require("express");
+// const { getToDo, saveToDo, updateToDo, deleteToDo } = require("../controllers/ToDoController");
+
+// const router = Router();
+
+
+// router.get('/',getToDo)
+// router.get('/save',saveToDo)
+// router.get('/update',updateToDo)
+// router.get('/delete',deleteToDo)
+
+// module.exports = router;
